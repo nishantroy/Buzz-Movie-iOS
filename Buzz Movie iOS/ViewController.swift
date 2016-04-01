573c23e14c69a26ecb24a4b37c5c5eafac207ba0
Nishant Roy <nroy6@gatech.edu>
2016-04-01 13:46:37 -0400

    Initial Commit

---
+++ Buzz Movie iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buzz Movie iOS
+//
+//  Created by localadmin on 4/1/16.
+//  Copyright © 2016 2b||!2b. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Buzz Movie iOSTests/Buzz_Movie_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Buzz_Movie_iOSTests.swift
+//  Buzz Movie iOSTests
+//
+//  Created by localadmin on 4/1/16.
+//  Copyright © 2016 2b||!2b. All rights reserved.
+//
+
+import XCTest
+@testable import Buzz_Movie_iOS
+
+class Buzz_Movie_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
