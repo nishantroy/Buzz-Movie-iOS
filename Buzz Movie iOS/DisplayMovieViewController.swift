4aafd9dbe16445a071eb5341c626e3b8f2833baa
Nishant Roy <nroy6@gatech.edu>
2016-04-14 15:48:28 -0400

    User ratings saved and retrieved if he searches for a movie he's rated before.

--- Buzz Movie iOS/DisplayMovieViewController.swift
+++ Buzz Movie iOS/DisplayMovieViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import SwiftyJSON
 
 class DisplayMovieViewController: UIViewController {
     
@@ -18,7 +20,7 @@ class DisplayMovieViewController: UIViewController {
     var imagePath = String()
     var userRating = Int()
     
-
+    
     @IBOutlet weak var movieRating: UILabel!
     @IBOutlet weak var moviePlot: UITextView!
     @IBOutlet weak var moviePicture: UIImageView!
@@ -29,12 +31,13 @@ class DisplayMovieViewController: UIViewController {
         return moviePlot.font!.lineHeight * amountOfLinesToBeShown
     }
     
-   
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Display Movie"
-
+        checkIfRated(name)
+        
         // Do any additional setup after loading the view.
     }
     
@@ -47,34 +50,52 @@ class DisplayMovieViewController: UIViewController {
         if let url = NSURL(string: imagePath) {
             if let data = NSData(contentsOfURL: url) {
                 moviePicture.image = UIImage(data: data)
-            }        
+            }
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     //MARK: Actions
     
     @IBAction func backToSearch(sender: UIBarButtonItem) {
         self.userRating = ratingControl.rating
         //TODO: Add child for every user in Firebase tracking which movies they've rated and how much they rated it, if rating > 0 only.
-        self.view.window!.makeToast("You rated this movie as a \(self.userRating)", duration: 1, position: .Bottom)
+        
+        if (self.userRating > 0) {
+            let movie = ["User Rating": self.userRating, "IMDB Rating": self.imdbRating, "Image": self.imagePath]
+            currentUserMovies.updateChildValues([self.name: movie])
+            self.view.window!.makeToast("You rated this movie as a \(self.userRating)", duration: 1, position: .Bottom)
+        }
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
-
+    func checkIfRated(movieName: String) {
+        currentUserMovies.observeEventType(.Value) { (snap: FDataSnapshot!) in
+            let json = JSON(snap.value)
+            if (json[self.name] != JSON.null) {
+                print("Found!")
+                let rating = json[self.name]["User Rating"].int!
+                self.ratingControl.rating = rating
+            } else {
+                print("Not yet rated!")
+            }
+        }
+    }
+    
+    
 }
