e43a09f6160c9483fd966074ef6c42b8ec8c11a4
Nishant Roy <nroy6@gatech.edu>
2016-04-22 15:04:22 -0400

    Password length check (at least 8 chars)

--- Buzz Movie iOS/CreateAccountViewController.swift
+++ Buzz Movie iOS/CreateAccountViewController.swift
@@ -41,55 +41,64 @@ class CreateAccountViewController: UIViewController, UITextFieldDelegate {
         let usersRef = BASE_REF.childByAppendingPath("users")
         
         if (email != "" && password != "" && name != "" && major != "") {
-            
-            
-            
-            BASE_REF.createUser(email, password: password, withValueCompletionBlock: { (error, result) -> Void in
+            if (password?.characters.count >= 8) {
                 
-                if (error == nil) {
-                    BASE_REF.authUser(email, password: password, withCompletionBlock: { (error, authData) -> Void in
-                        if (error == nil) {
-                            NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: "uid")
-                            
-                            print("Account created :)")
-                            
-                            let userDict = ["Name": name!, "Major": major!, "Email": email!]
-                            
-                            let uid = authData.uid
-                            
-                            usersRef.childByAppendingPath(uid).setValue(userDict)
-                            
-                            self.dismissViewControllerAnimated(true, completion: nil)
-                            
-                        } else {
-                            print(error)
-                        }
-                    })
+                
+                BASE_REF.createUser(email, password: password, withValueCompletionBlock: { (error, result) -> Void in
                     
-                } else {
-                    if let errorCode = FAuthenticationError(rawValue: error.code) {
-                        switch (errorCode) {
-                        case .EmailTaken:
-                            let alert = UIAlertController(title: "Error", message: "Email is already taken!", preferredStyle: .Alert)
-                            
-                            let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
-                            
-                            alert.addAction(action)
-                            
-                            self.presentViewController(alert, animated: true, completion: nil)
-                            
-                        default:
-                            let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .Alert)
-                            
-                            let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
-                            
-                            alert.addAction(action)
-                            
-                            self.presentViewController(alert, animated: true, completion: nil)
+                    if (error == nil) {
+                        BASE_REF.authUser(email, password: password, withCompletionBlock: { (error, authData) -> Void in
+                            if (error == nil) {
+                                NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: "uid")
+                                
+                                print("Account created :)")
+                                
+                                let userDict = ["Name": name!, "Major": major!, "Email": email!]
+                                
+                                let uid = authData.uid
+                                
+                                usersRef.childByAppendingPath(uid).setValue(userDict)
+                                
+                                self.dismissViewControllerAnimated(true, completion: nil)
+                                
+                            } else {
+                                print(error)
+                            }
+                        })
+                        
+                    } else {
+                        if let errorCode = FAuthenticationError(rawValue: error.code) {
+                            switch (errorCode) {
+                            case .EmailTaken:
+                                let alert = UIAlertController(title: "Error", message: "Email is already taken!", preferredStyle: .Alert)
+                                
+                                let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                                
+                                alert.addAction(action)
+                                
+                                self.presentViewController(alert, animated: true, completion: nil)
+                                
+                            default:
+                                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .Alert)
+                                
+                                let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                                
+                                alert.addAction(action)
+                                
+                                self.presentViewController(alert, animated: true, completion: nil)
+                            }
                         }
                     }
-                }
-            })
+                })
+            } else {
+                let alert = UIAlertController(title: "Error", message: "Password must be at least 8 characters long!", preferredStyle: .Alert)
+                
+                let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                
+                alert.addAction(action)
+                
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
         } else {
             let alert = UIAlertController(title: "Error", message: "Enter email, password, name and major", preferredStyle: .Alert)
             
